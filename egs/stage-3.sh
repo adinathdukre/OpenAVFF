287ef3c4603ad1575c95e6d5e910c3100d17d6e7
adinathdukre <121380724+adinathdukre@users.noreply.github.com>
2025-05-30 02:25:03 +0400

    Update stage-3.sh

--- egs/stage-3.sh
+++ egs/stage-3.sh
@@ -16,7 +16,7 @@ mae_loss_weight=1.0
 norm_pix_loss=True
 
 # you can use any checkpoints with a decoder, but by default, we use vision-MAE checkpoint
-pretrain_path=/l/PathoGen/Adinath/OpenAVFF/stage-3.pth
+# pretrain_path=/l/PathoGen/Adinath/OpenAVFF/stage-3.pth
 
 lr=1e-5
 head_lr=50
@@ -35,8 +35,8 @@ lr_adapt=False
 
 n_print_steps=100
 
-tr_data=/l/PathoGen/Adinath/train_videos_labels.csv
-te_data=/l/PathoGen/Adinath/val_videos_labels.csv
+tr_data=/l/PathoGen/Adinath/train_videos_labels_MC.csv
+te_data=/l/PathoGen/Adinath/val_videos_labels_MC.csv
 
 # exp_dir=./exp/self-pretrain
 save_dir=./exp/stage-3
@@ -44,14 +44,14 @@ mkdir -p $save_dir
 mkdir -p ${save_dir}/models
 
 CUDA_CACHE_DISABLE=1 CUDA_VISIBLE_DEVICES=0,1,2,3, python -W ignore ../src/run_ft.py \
---data-train ${tr_data} --data-val ${te_data} --save-dir $save_dir --n_classes 2 \
+--data-train ${tr_data} --data-val ${te_data} --save-dir $save_dir --n_classes 8 \
 --lr $lr --n-epochs ${epoch} --batch-size $batch_size \
 --lrscheduler_start ${lrscheduler_start} --lrscheduler_decay ${lrscheduler_decay} --lrscheduler_step ${lrscheduler_step} \
 --dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} --noise ${noise} \
 --lr_adapt ${lr_adapt} \
 --norm_pix_loss ${norm_pix_loss} \
 --mae_loss_weight ${mae_loss_weight} --contrast_loss_weight ${contrast_loss_weight} \
---loss BCE --metrics mAP --warmup True \
+--loss CE --metrics acc --warmup True \
 --wa_start ${wa_start} --wa_end ${wa_end} --lr_adapt ${lr_adapt} \
 --head_lr ${head_lr} \
---pretrain_path ${pretrain_path} --num_workers 32\
+--num_workers 32\
